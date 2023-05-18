dbc910ea250f7a8bad4a9ffd489473cee642ad65
PinkPlush <raevschicatalin@gmail.com>
2023-05-18 11:45:30 +0300

    full functionality, will add the readme later

--- your-project-name/src/Components/Converter.jsx
+++ your-project-name/src/Components/Converter.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Select from 'react-select';
 import Api from './Api';
 
@@ -9,11 +10,6 @@ const Converter = () => {
   const [chartData, setChartData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [conversionResult, setConversionResult] = useState('');
-
-  const onDataProcessed = (processedData) => {
-    setChartData(processedData);
-    setIsLoading(false);
-  };
 
   useEffect(() => {
     const savedData = localStorage.getItem('chartData');
@@ -27,6 +23,12 @@ const Converter = () => {
       localStorage.setItem('chartData', JSON.stringify(chartData));
     }
   }, [chartData]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -45,10 +47,10 @@ const Converter = () => {
       alert('Please select currencies and enter an amount.');
       return;
     }
-  
+
     const fromCurrency = selectedFromCurrency.value;
     const toCurrency = selectedToCurrency.value;
-    
+
     if (fromCurrency === 'RON') {
       const toRate = chartData.values[chartData.labels.indexOf(toCurrency)];
       const convertedAmount = inputValue / toRate;
@@ -64,12 +66,11 @@ const Converter = () => {
       setConversionResult(`${inputValue} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`);
     }
   };
-  
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="text-2xl font-bold text-center mb-6">Currency Converter</h2>
-  
+
       {isLoading ? (
         <p>Loading data...</p>
       ) : (
@@ -82,7 +83,7 @@ const Converter = () => {
               value={inputValue}
               onChange={handleInputChange}
             />
-  
+
             <Select
               className="flex-grow ml-4"
               placeholder="From currency"
@@ -96,7 +97,7 @@ const Converter = () => {
                 })),
               ]}
             />
-  
+
             <Select
               className="flex-grow ml-4"
               placeholder="To currency"
@@ -111,33 +112,36 @@ const Converter = () => {
               ]}
             />
           </div>
-  
+
           <button
             className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
             onClick={handleConversion}
           >
             Convert
           </button>
-  
-          {conversionResult && (
-            <p className="mt-4">{conversionResult}</p>
-          )}
-  
+
+          {conversionResult && <p className="mt-4">{conversionResult}</p>}
+
           <div className="cta-buttons mt-8">
-            <Link to="/exchange-chart" className="cta-button bg-teal-200 hover:bg-teal-300 text-teal-900 hover:text-white px-4 py-2 rounded-md transition-colors duration-300 mr-8">
+            <Link
+              to="/exchange-chart"
+              className="cta-button bg-teal-200 hover:bg-teal-300 text-teal-900 hover:text-white px-4 py-2 rounded-md transition-colors duration-300 mr-8"
+            >
               Exchange Rate Chart
             </Link>
-            <Link to="/valute-converter" className="cta-button bg-teal-200 hover:bg-teal-300 text-teal-900 hover:text-white px-4 py-2 rounded-md transition-colors duration-300">
+            <Link
+              to="/valute-converter"
+              className="cta-button bg-teal-200 hover:bg-teal-300 text-teal-900 hover:text-white px-4 py-2 rounded-md transition-colors duration-300"
+            >
               Currency Converter
             </Link>
           </div>
         </>
       )}
-  
-      <Api onDataProcessed={onDataProcessed} />
+
+      <Api onDataProcessed={setChartData} />
     </div>
   );
-  
 };
 
 export default Converter;
